ce10ce69429ed184ba3a65a59fe37228c86abcfe
Maverick Tesini <mavericktesini@gmail.com>
2018-08-14 22:30:45 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,9 +1,8 @@
-function produceDrivingRange(driveRange) {
-  return function drivingRange(blockRange){
-    let bR = parseInt(blockRange, 10);
-    // let bR = blockRange;
-    // let dR = parseInt(driveRange, 10);
-    let dR = driveRange;
+function produceDrivingRange(blockRange) {
+  return function drivingRange(driveRange){
+    // let bR = parseInt(blockRange, 10);
+    let bR = blockRange;
+    let dR = parseInt(driveRange, 10);
     if(dR > bR) {
       let difference = dR - bR;
       return `${difference} blocks out of range`
