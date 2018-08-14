20cb85ba1f6ad15bbd3e4e733ab3cb9a516e3255
Maverick Tesini <mavericktesini@gmail.com>
2018-08-14 22:17:26 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
 function produceDrivingRange(blockRange) {
   return function drivingRange(driveRange){
     if(driveRange > blockRange) {
-      let difference = (driveRange - blockRange);
+      let difference = driveRange - blockRange;
+      debugger;
       return `${difference} blocks out of range`
     } else {
-      let difference = (blockRange - driveRange);
+      let difference = blockRange - driveRange;
       return `within range by ${difference}`
     }
   }
